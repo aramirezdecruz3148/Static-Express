b41cac8868d3c56e52c84d50a5ea090613b6bf98
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-10 21:13:36 -0700

    I realized I wasn't displaying the content the way I intended in color.js, so I made a few changes

--- public/js/color.js
+++ public/js/color.js
@@ -2,14 +2,17 @@ import { getColorDetail } from './services/colorsApi.js';
 
 const mainContent = document.getElementById('root');
 const h3 = document.createElement('h3');
+const h2 = document.createElement('h2');
 
 const search = new URLSearchParams(window.location.search);
 const name = search.get('name');
 
 getColorDetail(name)
   .then(color => {
-    h3.textContent = `${color.name}: HEX-${color.hex}, RGB-${color.r, color.g, color.b}`;
+    h2.textContent = `${color.name}`;
+    h3.textContent = `#${color.hex}, rgb(${color.r}, ${color.g}, ${color.b})`;
   });
   
+mainContent.appendChild(h2);
 mainContent.appendChild(h3);
 
